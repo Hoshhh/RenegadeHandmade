abacb93bebccb560da0dd79bab682f8b1149be42
Hoshhh <nyxious123@live.com>
2022-05-16 22:18:05 -0400

    trying transitions on logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import '../src/app.css'
 
 function App() {
   return (
     <div className="App">
-      yo
+      <div className='container'>
+        <img src="/images/Black_on_Transparent.png" alt="" className="logo"/>
+      </div>
     </div>
   );
 }
