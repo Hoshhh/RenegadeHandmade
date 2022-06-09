0ac619458041e6101095793cb99493de85347417
Hoshhh <nyxious123@live.com>
2022-06-09 00:20:11 -0400

    More Hero work, making things mobile responsive. Working on color schemes as well

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <Stack direction="row" justifyContent="center" sx={{ marginLeft: "10%", marginRight: "10%"}}>
+    <Stack direction="row" justifyContent="center" sx={{ marginLeft: "10%", marginRight: "10%" }}>
         <Box flex={5} bgcolor="red" padding={2}>CTA</Box>
-        <Box 
-            flex={5} 
-            bgcolor="blue" 
-            padding={2} 
-            sx={{ display: { xs: "none", sm: "block"} }}>
-        Image</Box>
+        <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ display: { xs: "none", sm: "none", md: "flex"}, minHeight: "90vh" }}>
+          <img src="Images/Black_on_Transparent.png" alt="" style={{ maxWidth: "100%", margin: "auto auto" }}/>
+        </Box>
     </Stack>
   )
 }
