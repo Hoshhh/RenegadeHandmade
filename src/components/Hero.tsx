2918e560b9ae475efeb4e83324943a6077255f8d
Hoshhh <nyxious123@live.com>
2022-06-10 19:54:14 -0400

    Trying out different styling for the navbar, and started doing more Hero section work

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,19 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Button, Stack, styled, Typography } from '@mui/material'
 import React from 'react'
+
+const Buttons = styled(Button)({
+  marginLeft: "10px",
+  backgroundColor: "black"
+})
 
 const Hero = () => {
   return (
     <Stack direction="row" justifyContent="center" sx={{ marginLeft: "10%", marginRight: "10%" }}>
-        <Box flex={5} bgcolor="red" padding={2}>CTA</Box>
+        <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ margin: "auto auto", textAlign: "center"}}>
+          <Typography variant='h1'>One-of-a-kind, Handmade Furniture</Typography>
+          <Buttons variant='contained' >Browse</Buttons>
+          <Buttons variant='contained'>Custom Work</Buttons>
+        </Box>
         <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ display: { xs: "none", sm: "none", md: "flex"}, minHeight: "90vh" }}>
           <img src="Images/Black_on_Transparent.png" alt="" style={{ maxWidth: "100%", margin: "auto auto" }}/>
         </Box>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Facebook, ShoppingCart } from '@mui/icons-material'
-import { AppBar, Badge, Box, styled, Toolbar, Typography } from '@mui/material'
+import { AppBar, Badge, Box, Button, styled, Toolbar, Typography } from '@mui/material'
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -8,19 +8,22 @@ const StyledToolbar = styled(Toolbar)({
 
 const Icons = styled(Box)({
   display: "flex",
-  gap: "20px"
+  gap: "40px",
+  marginLeft: "40px",
+  marginRight: "20px"
 })
 
 const Navbar = () => {
   return (
-    <AppBar position="sticky">
+    <AppBar position="sticky" sx={{ backgroundColor: "rgba(255,255,255,0)", boxShadow: "none"}}>
       <StyledToolbar>
-        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block"} }}>Renegade Handmade</Typography>
-        <Typography variant="h6" sx={{ display: { xs: "block", sm: "none"} }}>Renegade</Typography>
+        <Typography variant="h6" color="black" sx={{ display: { xs: "none", sm: "block"}, marginLeft: "20px" }}>Renegade Handmade</Typography>
+        <Typography variant="h6" color="black" sx={{ display: { xs: "block", sm: "none"}, marginLeft: "20px" }}>Renegade</Typography>
+        <Button variant="contained" size="small" sx={{ margin: "auto", marginRight: "0px", backgroundColor: "black"}}>Contact</Button>
         <Icons>
-          <Facebook fontSize="large"/>
+          <Facebook fontSize="large" htmlColor='black'/>
           <Badge badgeContent={3} color="error">
-            <ShoppingCart fontSize="large" />
+            <ShoppingCart fontSize="large" htmlColor='black'/>
           </Badge>
         </Icons>
       </StyledToolbar>
