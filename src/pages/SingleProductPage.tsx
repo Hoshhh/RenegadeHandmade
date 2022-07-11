87eeda9232190384fb419ae7c757d717dec149bc
Hoshhh <nyxious123@live.com>
2022-07-11 16:58:36 -0400

    Worked on a more consistent size for the images on the single product pages

--- src/pages/SingleProductPage.tsx
+++ src/pages/SingleProductPage.tsx
@@ -17,22 +17,22 @@ const SingleProductPage = (props: ProductProps) => {
 
     console.log(props.products[id])
   return (
-    <Stack direction="row" justifyContent="center" sx={{ marginLeft: "5%", marginRight: "5%", marginTop: "3%" }}>
-        <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ display: { xs: "none", sm: "none", md: "flex"}, minHeight: "90vh", flexDirection: "column" }}>
-            <img src={image} alt="" style={{ maxWidth: "100%", marginBottom: "5%", marginTop: "5%" }}/>
+    <Stack direction="row" justifyContent="center" sx={{ marginLeft: "5%", marginRight: "5%", marginTop: "0px" }}>
+        <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ display: { xs: "none", sm: "none", md: "flex"}, minHeight: "90vh", flexDirection: "column", justifyContent:"center", alignItems: "center" }}>
+            <img src={image} alt="" style={{ maxWidth: "75%",  maxHeight: "500px", marginBottom: "5%", marginTop: "5%", marginLeft: "auto", marginRight: "auto" }}/>
             <Container disableGutters sx={{ display: "block"}}>
-                <Grid container spacing={2}>
+                <Grid container spacing={0} sx={{ display: "flex", justifyContent: "center"}}>
                     {
                         props.products[id].assets.map((photo) => (
                             <Grid item xs={12} sm={6} md={4} lg={3}>
-                                <img src={photo.url} alt="" onClick={() => handleImageChange(photo.url)} style={{ maxWidth: "100%", marginBottom: "5%", border: image === photo.url ? "3px solid #1abc9c": "" }}/>
+                                <img src={photo.url} alt="" onClick={() => handleImageChange(photo.url)} style={{ maxWidth: "80%", maxHeight: "121px", marginBottom: "5%", border: image === photo.url ? "3px solid #1abc9c": "" }}/>
                             </Grid>
                         ))
                     }
                 </Grid>
             </Container>
         </Box>
-        <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ display: "flex", minHeight: "90vh", flexDirection: "column" }}>
+        <Box flex={5} bgcolor="#F1EFF1" padding={2} sx={{ display: "flex", minHeight: "90vh", flexDirection: "column", justifyContent:"center" }}>
             <Typography variant="h4" style={{ maxWidth: "100%", marginTop: "5%", marginBottom: "0.8em" }}>{props.products[id].name}</Typography>
             <Typography variant="body1" dangerouslySetInnerHTML={{ __html: props.products[id].description}}  style={{ marginBottom: "0.8em" }}/>
             <Typography variant="h5" style={{ marginBottom: "0.8em" }}>{props.products[id].price.formatted_with_symbol}</Typography>
